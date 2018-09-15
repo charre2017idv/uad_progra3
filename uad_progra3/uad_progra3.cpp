341482c5c127f178df08a7624dd8b647cfe65c4e
charre2017idv <idv17c.rcharreton@uartesdigitales.edu.mx>
2018-09-14 20:58:33 -0500

    Esfera
    Se realizo la tarea de la esfera

--- uad_progra3/uad_progra3.cpp
+++ uad_progra3/uad_progra3.cpp
@@ -9,13 +9,14 @@ using namespace std;
 #include "Include/CGameWindow.h"
 #include "Include/CApp.h"
 #include "Include/CAppParcial2.h"
-#include "Include/CAppHexGrid.h"
+#include "Include/CGeometricFigure.cpp.h"
+//#include "Include/CAppHexGrid.h"
 #include "Include/CAppEmpty.h"
 
 int main()
 {
 	CApp *app = NULL;                  // Pointer to BASE class CApp
-	app = new CAppEmpty(800, 600);     // Using pointer to base class, create a new object of DERIVED class
+	app = new CGeometricFigure(800, 600);     // Using pointer to base class, create a new object of DERIVED class
 	app->run();                        // Run the app
 	delete app;                        // Delete pointer
 	app = NULL;                        // Set pointer to NULL
